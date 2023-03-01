bfe790b075c1ddac70e94972b69a10da628dc830
atulsingh369 <businessatul7684@gmail.com>
2023-03-01 13:04:10 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+let seconds = 0;
+var time = [];
+
+const disp = () => {
+	let t = 0;
+	time[0] = Math.floor(seconds / 3600);
+	time[1] = Math.floor((seconds % 3600) / 60);
+	time[2] = Math.floor(seconds % 3600 % 60);
+	for (i = 0; i < 3; i++) {
+		if (Math.floor(time[i] / 10) == 0) {
+			time[i].toString();
+			time[i] = t.toString() + time[i];
+		}
+	}
+	const idS = ["hrs", "min", "sec"];
+	for (i = 0; i < 3; i++) {
+		document.getElementById(idS[i]).textContent = time[i];
+	}
+}
+
 const start = () => {
-	console.log("start");
+	id = window.setInterval(function () {
+		seconds++;
+		disp();
+	}, 1000);
 }
 
 const stop = () => {
-	console.log("stop");
+	clearInterval(id);
 }
